d6896c4b78ad6b94dce398873e1597c8734a9411
t <shaprina.tatiana888@gmail.com>
2025-10-16 10:11:58 +0300

    Initial commit

---
+++ config.json/1.py
@@ -0,0 +1,5 @@
+{
+  "TELEGRAM_API_KEY": "<your_telegram_api_key>",
+  "GIGACHAT_API_URL": "https://api.gigachat.ru/v1/completions",
+  "YANDEX_SPEECHKIT_API_KEY": "<your_yandex_speechkit_api_key>"
+}
